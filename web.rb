8d5b90fae3b4d07b39dde7e49e0c712ea5b0e81e
yyjim <yy.jim731@gmail.com>
2013-11-03 02:26:45 +0800

    updated token

--- web.rb
+++ web.rb
@@ -6,8 +6,8 @@ require 'tempfile'
 FlickRaw.api_key="284b61e98a52a83778c61f66d7e4a3f1"
 FlickRaw.shared_secret="8d41899bad974b8e"
 
-ACCESS_TOKEN = "72157637212673085-3980068d878cc8db" #yyjim
-ACCESS_SECRET = "4c133c01232e6c3a"                  
+ACCESS_TOKEN = "72157637230661926-6115cf7788a86f63" #fotoflick_tester@yahoo.com.tw pw4
+ACCESS_SECRET = "bc6fc4fda7cd78b8"                  
 
 get '/' do
   "Hello, world"
